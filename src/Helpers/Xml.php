a8279e7d2650dfc695cf9872beba1ca62401b8a2
guillermoandrae <guillermo.fisher@cloudreach.com>
2018-06-22 10:23:51 -0700

    Added the XML helper.

---
+++ src/Helpers/Xml.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Guillermoandrae\Highrise\Helpers;
+
+use Guillermoandrae\Common\Collection;
+use Guillermoandrae\Common\CollectionInterface;
+use SimpleXMLElement;
+
+final class Xml
+{
+    /**
+     * Converts an XML string into either an associative array or an array of
+     * associative arrays.
+     *
+     * @param string $xml The XML string.
+     * @return array|CollectionInterface
+     */
+    public static function parse(string $xml)
+    {
+        $obj = simplexml_load_string($xml);
+        if ($obj->attributes()) {
+            $items = [];
+            foreach ($obj->children() as $child) {
+                $items[] = static::flatten($child);
+            }
+            return Collection::make($items);
+        }
+        return (array) static::flatten($obj);
+    }
+
+    /**
+     * Removes attributes from SimpleXMLElement objects.
+     *
+     * @param SimpleXMLElement $item The SimpleXMLElement object.
+     * @return array
+     */
+    public static function flatten(SimpleXMLElement $item)
+    {
+        $parsedItem = [];
+        foreach ($item as $key => $value) {
+            $parsedItem[$key] = (string) $value;
+        }
+        return $parsedItem;
+    }
+}
