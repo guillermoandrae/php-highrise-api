74a739e03df19f54f3f0c2905e513a967da9663e
guillermoandrae <guillermo.fisher@cloudreach.com>
2018-06-23 20:38:49 -0700

    Methods for array <=> xml.

--- src/Helpers/Xml.php
+++ src/Helpers/Xml.php
@@ -9,13 +9,31 @@ use SimpleXMLElement;
 final class Xml
 {
     /**
+     * Converts a one-dimensional associative array to an XML string using the
+     * provided data.
+     *
+     * @param string $root  The name of the root element.
+     * @param array $data  The XML data.
+     * @return string
+     */
+    public static function toXml(string $root, array $data)
+    {
+        $body = sprintf('<%s>', $root);
+        foreach ($data as $key => $value) {
+            $body .= sprintf('<%s>%s</%s>', $key, $value, $key);
+        }
+        $body .= sprintf('</%s>', $root);
+        return $body;
+    }
+
+    /**
      * Converts an XML string into either an associative array or an array of
      * associative arrays.
      *
      * @param string $xml The XML string.
      * @return array|CollectionInterface
      */
-    public static function parse(string $xml)
+    public static function fromXml(string $xml)
     {
         libxml_use_internal_errors(true);
         if (!$obj = simplexml_load_string($xml)) {
