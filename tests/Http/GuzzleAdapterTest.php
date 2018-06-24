aa5072b0e807b8e27bc60c709c4296dac69ec97c
guillermoandrae <guillermo.fisher@cloudreach.com>
2018-06-23 20:41:52 -0700

    Major improvements to the tests.

--- tests/Http/GuzzleAdapterTest.php
+++ tests/Http/GuzzleAdapterTest.php
@@ -2,7 +2,6 @@
 
 namespace GuillermoandraeTest\Highrise\Http;
 
-use Guillermoandrae\Highrise\Http\AdapterInterface;
 use Guillermoandrae\Highrise\Http\GuzzleAdapter;
 use Guillermoandrae\Highrise\Http\RequestException;
 use GuzzleHttp\Client;
@@ -13,48 +12,57 @@ use PHPUnit\Framework\TestCase;
 
 class GuzzleAdapterTest extends TestCase
 {
-    /**
-     * @var AdapterInterface
-     */
-    private $adapter;
-
     public function testRequestWithBody()
     {
         $body = '<test><name>test</name></test>';
-        $this->assertRequest(200, 'GET', [], $body);
+        $client = $this->getGuzzleClient(201);
+        $adapter = $this->getGuzzleAdapter($client);
+        $adapter->request('GET', '/test.xml', ['body' => $body]);
+        $this->assertSame($body, (string) $adapter->getLastRequest()->getBody());
     }
 
     public function testRequestWithoutBody()
     {
-        $this->assertRequest(201, 'DELETE');
+        $client = $this->getGuzzleClient(200);
+        $adapter = $this->getGuzzleAdapter($client);
+        $adapter->request('DELETE', '/test.xml');
+        $this->assertEmpty((string) $adapter->getLastRequest()->getBody());
+    }
+
+    public function testRequestWithHeadersWithoutBody()
+    {
+        $client = $this->getGuzzleClient(200);
+        $adapter = $this->getGuzzleAdapter($client);
+        $adapter->request('GET', '/test.xml', ['headers' => ['X-Test' => 'test']]);
+        $this->assertArrayHasKey('X-Test', $adapter->getLastRequest()->getHeaders());
     }
 
     public function testInvalidRequestWithoutBody()
     {
         $this->expectException(RequestException::class);
-        $this->assertRequest(503, 'GET');
+        $client = $this->getGuzzleClient(503);
+        $adapter = $this->getGuzzleAdapter($client);
+        $adapter->request('DELETE', '/test.xml');
     }
 
     public function testGetClient()
     {
-        $this->assertInstanceOf(Client::class, $this->adapter->getClient());
+        $this->assertInstanceOf(Client::class, (new GuzzleAdapter('a', 'b'))->getClient());
     }
 
-    protected function setUp()
+    private function getGuzzleAdapter(Client $client): GuzzleAdapter
     {
-        $this->adapter = new GuzzleAdapter('test', '123456');
+        $adapter = new GuzzleAdapter('test', '123456');
+        $adapter->setClient($client);
+        return $adapter;
     }
 
-    private function assertRequest($httpStatusCode, string $method, array $options = [], $body = '')
+    private function getGuzzleClient($expectedHttpStatusCode, array $expectedHeaders = [], $expectedBody = ''): Client
     {
-        $uri = '/test.xml';
         $mock = new MockHandler([
-            new Response($httpStatusCode, [], $body)
+            new Response($expectedHttpStatusCode, $expectedHeaders, $expectedBody)
         ]);
         $handler = HandlerStack::create($mock);
-        $this->adapter->setClient(new Client(['handler' => $handler]));
-        $this->adapter->request($method, $uri, $options);
-        $this->assertSame($uri, $this->adapter->getLastRequest()->getUri()->getPath());
-        $this->assertSame($body, (string) $this->adapter->getLastResponse()->getBody());
+        return new Client(['handler' => $handler]);
     }
 }
