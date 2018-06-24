297dec28690b30f147024ffbb601abe55ba12849
guillermoandrae <guillermo.fisher@cloudreach.com>
2018-06-23 20:40:31 -0700

    Added specific XML header, handling headers and body in requests for retrieval later on.

--- src/Http/GuzzleAdapter.php
+++ src/Http/GuzzleAdapter.php
@@ -13,6 +13,7 @@ class GuzzleAdapter extends AbstractAdapter
             $this->setClient(new Client([
                 'base_uri' => sprintf('https://%s.highrisehq.com', $this->getSubdomain()),
                 'headers' => [
+                    'Content-Type' => 'application/xml',
                     'User-Agent' => static::HEADER_DEFAULT_USER_AGENT,
                 ],
                 'auth' => [$this->getToken(), $this->getPassword()]
@@ -23,7 +24,17 @@ class GuzzleAdapter extends AbstractAdapter
 
     protected function send(string $method, string $uri, array $options = [])
     {
-        $this->lastRequest = new Request($method, $uri);
+        $headers = [];
+        if (isset($options['headers'])) {
+            $headers = $options['headers'];
+            unset($options['headers']);
+        }
+        $body = '';
+        if (isset($options['body'])) {
+            $body = $options['body'];
+            unset($options['body']);
+        }
+        $this->lastRequest = new Request($method, $uri, $headers, $body);
         $this->lastResponse = $this->getClient()->send($this->lastRequest, $options);
     }
 }
